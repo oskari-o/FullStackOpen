ebbb9b0af1b8bb7fa600e7b0a7305ecc21de0898
jjooskari <jjooskari>
2023-10-29 22:49:14 +0200

    Ex 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,6 +9,19 @@ const Button = ({ state, setState, text }) => {
   )
 }
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all
+  return (
+    <>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -25,9 +38,7 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good/(good + neutral + bad)}</p>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
