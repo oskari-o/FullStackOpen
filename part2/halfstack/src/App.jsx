8f30305e30c33cfa34527fa60adcf9f3cb79ded6
jjooskari <jjooskari>
2023-11-02 17:08:20 +0200

    Refactor Ex 2.1

--- part2/halfstack/src/App.jsx
+++ part2/halfstack/src/App.jsx
@@ -55,22 +55,6 @@ const App = () => {
     ]
   }
 
-  const course_ = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
   return <Course course={course} />
 }
 
