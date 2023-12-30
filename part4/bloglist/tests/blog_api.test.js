24a17b9466d787ae304280d15eca459a8ca5fa3b
jjooskari <jjooskari>
2023-12-30 15:02:18 +0700

    Ex 4.8 - 4.10 patch

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -26,8 +26,31 @@ test('all blogs are returned', async () => {
 
 test('id property exists', async () => {
   const response = await api.get('/api/blogs')
-  console.log(response.body)
   expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Test2',
+    author: 'Test2',
+    url: 'Test2',
+    likes: 20000
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const blogsAtEnd = response.body
+  expect(blogsAtEnd).toHaveLength(2)
+
+  const titles = blogsAtEnd.map(b => b.title)
+  expect(titles).toContain(
+    'Test2'
+  )
 })
 
 afterAll(async () => {
