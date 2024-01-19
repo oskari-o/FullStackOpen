1ae64da543418f833063ccefaf30f7416dc2b7be
jjooskari <jjooskari>
2024-01-19 15:05:15 +0200

    Ex 6.2wwwww

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -7,21 +7,21 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const increment = (action) => {
     store.dispatch({
-      type: 'GOOD'
+      type: action
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => (increment('GOOD'))}>good</button> 
+      <button onClick={() => (increment('OK'))}>ok</button> 
+      <button onClick={() => (increment('BAD'))}>bad</button>
+      <button onClick={() => (increment('ZERO'))}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
