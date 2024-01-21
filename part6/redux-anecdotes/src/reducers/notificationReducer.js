0b685d05088812aecdc2a6d0cac55afb55cfe5bc
jjooskari <jjooskari>
2024-01-21 17:17:14 +0200

    Ex 6.12

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = 'Initial notification lol'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    setNotification(state, action) {
+      return action.payload
+    },
+    clearNotification(state, action) {
+      state, action
+      return ''
+    }
+  }
+})
+
+export const { setNotification, clearNotification } = notificationSlice.actions
+export default notificationSlice.reducer
